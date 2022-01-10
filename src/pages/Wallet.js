38c54d2fca0b2a97d0a3759fecfdc88bc2fb778f
vitor <vitorsbsilva@gmail.com>
2022-01-10 17:33:08 -0300

    Reqs. 2 e 3 - feitos! Faltam os dois ajustes de lint

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail: { email } } = this.props;
+    return (
+      <div>
+        <div>TrybeWallet</div>
+
+        <header>
+          <p data-testid="email-field">{ email }</p>
+
+          <p data-testid="total-field"> 0 </p>
+
+          <p data-testid="header-currency-field"> BRL </p>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
