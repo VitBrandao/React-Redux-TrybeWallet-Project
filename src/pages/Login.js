cb97e507dfdad71d39d4487f1576818c0b76fdb5
vitor <vitorsbsilva@gmail.com>
2022-01-10 15:48:48 -0300

    Req.1 - implementaçao de funçoes atreladas ao onChange dos inputs e ao onClick do botao. Criada logica de validacao do email

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,7 +5,52 @@ class Login extends React.Component {
     super();
     this.state = {
       isDisabled: true,
+      email: '',
+      isEmailValid: false,
     };
+
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
+
+  onEmailChange({ target }) {
+    this.setState({
+      email: target.value,
+    });
+
+    // Encontrei essa forma de validar email aqui:
+    // https://www.w3resource.com/javascript/form/email-validation.php
+    const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(target.value.match(mailformat)) {
+      this.setState({
+        isEmailValid: true,
+      }) 
+    } else {
+      this.setState({
+        isEmailValid: false,
+        isDisabled: true,
+      })
+    }
+  }
+
+  onPasswordChange({ target }) {
+    const { isEmailValid } = this.state;
+    const minLength = 6;
+    if(target.value.length >= minLength && isEmailValid === true){
+      this.setState({
+        isDisabled: false,
+      });
+    } else {
+      this.setState({
+        isDisabled: true,
+      });
+    }
+  }
+
+  onButtonClick() {
+    const { history } = this.props;
+    history.push("/carteira");
   }
 
   render() {
@@ -16,15 +61,29 @@ class Login extends React.Component {
 
         <label>
           E-mail:
-          <input type='text' data-testid="email-input" />
+          <input 
+            type='email' 
+            data-testid="email-input"
+            onChange={ this.onEmailChange }
+          />
         </label>
 
         <label>
           Senha:
-          <input type='text' data-testid="password-input" />
+          <input 
+            type='password' 
+            data-testid="password-input"
+            onChange={this.onPasswordChange}  
+          />
         </label>
 
-        <button type='submit' disabled={ isDisabled }>Entrar</button>
+        <button 
+          type='submit' 
+          disabled={ isDisabled }
+          onClick={ this.onButtonClick }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
