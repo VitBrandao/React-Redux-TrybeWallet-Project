b28000a05edc31076c0419bb902f34b49d6d8f9d
vitor <vitorsbsilva@gmail.com>
2022-01-10 15:11:56 -0300

    Req.1 - criada rota padrao, inputs e botao da pagina Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { isDisabled } = this.state;
+    return (
+      <div>
+        <h1>LOGIN</h1>
+
+        <label>
+          E-mail:
+          <input type='text' data-testid="email-input" />
+        </label>
+
+        <label>
+          Senha:
+          <input type='text' data-testid="password-input" />
+        </label>
+
+        <button type='submit' disabled={ isDisabled }>Entrar</button>
+      </div>
+    );
   }
 }
 
